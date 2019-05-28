57dbc4e1800be9ed81a038f542c710550d1fb1d9
keshavkummari <keshav.kummari@gmail.com>
2019-05-28 07:57:03 +0530

    DataTypes

---
+++ DataTypes/String/Strings.py
@@ -0,0 +1,18 @@
+# Python Strings:
+
+'''
+Strings are amongst the most popular types in Python.
+We can create them simply by enclosing characters in quotes.
+Python treats single quotes the same as double quotes.
+Creating strings is as simple as assigning a value to a variable.
+'''
+
+# Example:
+
+#!/usr/bin/python
+
+course='python'
+course_1="python"
+
+print ("Single Quotes :, course")
+print ("Double Quotes :, course_1")
