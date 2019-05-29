8072ebfa2d284245474540c019b50af99233c064
hd8414 <hardikpatel19928@gmail.com>
2019-05-29 22:08:34 +0100

    creating variable and strings updates

---
+++ Creating a Variable.py
@@ -0,0 +1,38 @@
+'Creating Variable in Python'
+
+Course_name = 'Python Programming' # String Value
+Duration = "35 hours"
+Python_version = 3.7 # Floating Values
+Student_count = 10 # Integer Value
+Course_rating = 9.99 #Floating Value
+iS_course_started = True # Boolen Value
+is_published = False # Boolen Value
+
+message = '''
+Hi Python DeveTeam,
+
+Welcom to Python World!
+
+Thanks 
+HardIk Patel
+'''
+
+"""
+Accessing Variables in Python using built-in function called print()
+"""
+print(Course_name,type(Course_name),len(Course_name))
+
+print (Duration,type(Duration),len(Duration))
+
+print (Python_version,type(Python_version))
+
+print (Course_rating,type(Course_rating))
+
+print (Student_count,type(Student_count))
+
+print (Course_rating,type(Course_rating))
+
+print (iS_course_started,type(iS_course_started))
+
+print (is_published,type(is_published))
+
