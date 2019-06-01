662a471d00c3010ca63e4f565a20d065b0b495a4
hd8414 <hardikpatel19928@gmail.com>
2019-06-01 13:21:05 +0100

    center method

--- DataTypes/String/Center.py
+++ DataTypes/String/Center.py
@@ -1,12 +1,29 @@
-# center(width[, fillchar])
+# center() Method :
 
-''' Returns the string centered in a string of length width. P
-adding can be done using the specified
-fillchar (the default padding uses an ASCII space).
+''' Returns the string centered in a string of length width.
+Padding can be done using the specified
+fillchar (the default padding(filler) uses an space).
+
 The original string is returned if width is less than or equal to len(s)
+
+Syntax : str.center(width,fillchar)
+
+width -- This is the total width of the string
+fillchar -- This is the filler character.
+
 '''
 
 
-a = "bee"
-b = a.center(13, "#")
-print(b)
+str1 = "HARDIK"
+str2 = """PATEL \
+HARDIK \
+INDRAVADANBHAI\
+"""
+
+print ("lenght of str1 :",len(str1))
+print ("Ans of str1.center(17,'*') is : ",str1.center(17,'*'))
+print (" ")
+print ("length of str2 : ",len(str2))
+print ("Ans of str2.center(32,'#') is : ",str2.center(32,'#'))
+print("")
+print ("Ans of str1.center(4,'@') is : ",str1.center(4,'@')) # o/p will be sting itself as new string len is less than original string
