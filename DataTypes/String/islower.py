9d3f208866a0c59317071ea34684e0406e7a67ff
hd8414 <hardikpatel19928@gmail.com>
2019-06-01 13:56:04 +0100

    isupper and islower

---
+++ DataTypes/String/islower.py
@@ -0,0 +1,23 @@
+
+# islower () Method :
+
+'''
+Returns True if all cased characters in the string are
+lowercase and there is at least one cased character.
+Returns False otherwise.
+
+'''
+
+str1 = " LET'S WATCH CRICKET" # Full string in uppercase letter
+
+str2 = 'let\'s play football' # Full string in lowercase
+
+str3 = 'let\'s go For a walk' # 1 letter in uppercase
+
+print (str1.islower())
+print()  # 1 line space in o/p
+
+print(str2.islower())
+print ()
+
+print(str3.islower())
---
+++ DataTypes/String/isupper.py
@@ -0,0 +1,22 @@
+
+# isupper () Method :
+
+'''
+ Returns True if all cased characters in the string
+ are uppercase and there is at least one cased character.
+ Returns False otherwise.
+'''
+
+str1 = " LET'S WATCH CRICKET" # Full string in uppercase letter
+
+str2 = 'let\'s play football' # Full string in lowercase letter
+
+str3 = 'LET\'s GO FOR A WALK' # 1 letter in lowercase letter
+
+print (str1.isupper())
+print()  # 1 line space in o/p
+
+print(str2.isupper())
+print ()
+
+print(str3.isupper())
