1b462b9ecd8f54f3bce93d5dd5c7f8f58ba25b8b
keshavkummari <keshav.kummari@gmail.com>
2019-06-03 08:00:59 +0530

    StringMethods

--- DataTypes/String/Strings.py
+++ DataTypes/String/Strings.py
@@ -5,7 +5,7 @@ Strings are amongst the most popular types in Python.
 We can create them simply by enclosing characters in quotes.
 Python treats single quotes the same as double quotes.
 Creating strings is as simple as assigning a value to a variable.
-'''
+
 
 # Example:
 
@@ -16,3 +16,44 @@ course_1="python"
 
 print ("Single Quotes :, course")
 print ("Double Quotes :, course_1")
+
+'''
+
+# ljust(width[,fillchar])
+
+_007 = "James Bond"
+
+print(len(_007))
+
+print(_007.ljust(15,'*'))
+
+print(_007.rjust(15,'&'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
