ed956981a1e06e06be660c764aefa4da7a9ed113
keshavkummari <keshav.kummari@gmail.com>
2019-06-25 08:01:31 +0530

    Modules

---
+++ Modules/Overview_Of_Modules.py
@@ -0,0 +1,128 @@
+
+"""
+help()
+
+help> modules
+
+help> keywords
+
+help> symbols
+
+help> topics
+
+>>> help()
+
+help> modules
+
+Please wait a moment while I gather a list of all available modules...
+
+AutoComplete        _pyio               enum                pydoc
+AutoCompleteWindow  _random             errno               pydoc_data
+AutoExpand          _sha1               faulthandler        pyexpat
+Bindings            _sha256             file_keshav         queue
+CallTipWindow       _sha512             file_open_write_1   quopri
+CallTips            _signal             filecmp             random
+ClassBrowser        _sitebuiltins       fileinput           re
+CodeContext         _socket             fnmatch             reprlib
+ColorDelegator      _sqlite3            formatter           rlcompleter
+Debugger            _sre                fractions           rpc
+Delegator           _ssl                ftplib              run
+EditorWindow        _stat               functools           runpy
+FileList            _string             gc                  sched
+FormatParagraph     _strptime           genericpath         select
+GrepDialog          _struct             getopt              selectors
+HyperParser         _symtable           getpass             setuptools
+IOBinding           _testbuffer         gettext             shelve
+IdleHistory         _testcapi           glob                shlex
+MultiCall           _testimportmultiple gzip                shutil
+MultiStatusBar      _testmultiphase     hashlib             signal
+ObjectBrowser       _thread             heapq               site
+OutputWindow        _threading_local    help                smtpd
+ParenMatch          _tkinter            hmac                smtplib
+PathBrowser         _tracemalloc        html                sndhdr
+Percolator          _warnings           http                socket
+PyParse             _weakref            idle                socketserver
+PyShell             _weakrefset         idle_test           sqlite3
+RemoteDebugger      _winapi             idlelib             sre_compile
+RemoteObjectBrowser abc                 idlever             sre_constants
+ReplaceDialog       aboutDialog         imaplib             sre_parse
+RstripExtension     aifc                imghdr              ssl
+ScriptBinding       antigravity         imp                 stat
+ScrolledList        argparse            importlib           statistics
+SearchDialog        array               inspect             string
+SearchDialogBase    ast                 io                  stringprep
+SearchEngine        asynchat            ipaddress           struct
+StackViewer         asyncio             itertools           subprocess
+ToolTip             asyncore            json                sunau
+TreeWidget          atexit              keybindingDialog    symbol
+UndoDelegator       audioop             keyword             symtable
+WidgetRedirector    base64              lib2to3             sys
+WindowList          bdb                 linecache           sysconfig
+ZoomHeight          binascii            locale              tabbedpages
+__future__          binhex              logging             tabnanny
+__main__            bisect              lzma                tarfile
+_ast                builtins            macosxSupport       telnetlib
+_bisect             bz2                 macpath             tempfile
+_bootlocale         cProfile            macurl2path         test
+_bz2                calendar            mailbox             textView
+_codecs             cgi                 mailcap             textwrap
+_codecs_cn          cgitb               marshal             this
+_codecs_hk          chunk               math                threading
+_codecs_iso2022     cmath               mimetypes           time
+_codecs_jp          cmd                 mmap                timeit
+_codecs_kr          code                modulefinder        tkinter
+_codecs_tw          codecs              msilib              token
+_collections        codeop              msvcrt              tokenize
+_collections_abc    collections         multiprocessing     trace
+_compat_pickle      colorsys            netrc               traceback
+_compression        compileall          nntplib             tracemalloc
+_csv                concurrent          nt                  tty
+_ctypes             configDialog        ntpath              turtle
+_ctypes_test        configHandler       nturl2path          turtledemo
+_datetime           configHelpSourceEdit numbers             types
+_decimal            configSectionNameDialog opcode              typing
+_dummy_thread       configparser        operator            unicodedata
+_elementtree        contextlib          optparse            unittest
+_functools          copy                os                  urllib
+_hashlib            copyreg             parser              uu
+_heapq              crypt               pathlib             uuid
+_imp                csv                 pdb                 venv
+_io                 ctypes              pickle              warnings
+_json               curses              pickletools         wave
+_locale             datetime            pip                 weakref
+_lsprof             dbm                 pipes               webbrowser
+_lzma               decimal             pkg_resources       winreg
+_markupbase         difflib             pkgutil             winsound
+_md5                dis                 platform            wsgiref
+_msi                distutils           plistlib            xdrlib
+_multibytecodec     doctest             poplib              xml
+_multiprocessing    dummy_threading     posixpath           xmlrpc
+_opcode             dynOptionMenuWidget pprint              xxsubtype
+_operator           easy_install        profile             zipapp
+_osx_support        email               pstats              zipfile
+_overlapped         email_send          pty                 zipimport
+_pickle             encodings           py_compile          zlib
+_pydecimal          ensurepip           pyclbr              
+
+Enter any module name to get more help.  Or, type "modules spam" to search
+for modules whose name or summary contain the string "spam".
+
+import email
+
+from email import message
+
+from email import *
+
+pip search <module>
+
+pip download <module>
+
+pip install <module>
+"""
+
+
+
+
+
+
+
