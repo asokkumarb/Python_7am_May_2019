8cce48751bf9300b2714f5773b13b9b0a64bad31
keshavkummari <keshav.kummari@gmail.com>
2019-06-28 08:03:16 +0530

    GUI

---
+++ GUI/Button.py
@@ -0,0 +1,21 @@
+# Importing Modules and Classes from a Module
+from tkinter import *
+from tkinter import messagebox
+
+# Creating a Variable
+top = Tk()
+
+top.geometry("500x500")
+
+# Create a Function
+
+def helloCallBack():
+    msg = messagebox.showinfo("HOME","Welcome to Python GUI World")
+
+B = Button(top, text="CLICK TO LOGIN", command=helloCallBack)
+
+B.place(x=10,y=10)
+
+top.mainloop()
+
+
