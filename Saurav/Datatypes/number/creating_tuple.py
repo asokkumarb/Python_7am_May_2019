5ae27db56251b5faa09b37db6b5929d97c747cbb
Saurav5 <sauravdash45@gmail.com>
2019-11-19 00:35:21 +0530

    list_tuple

---
+++ Saurav/Datatypes/number/creating_tuple.py
@@ -0,0 +1,5 @@
+a = ("python", "aws", "devops", 5, 10.5, 3+2j, "$")
+
+
+
+print(list(a))
---
+++ Saurav/Datatypes/number/list_insert_medhos.py
@@ -0,0 +1,15 @@
+
+a = ["python", "aws", "devops", 5, 10.5, 3+2j, "$"]
+
+b = ["datascience"]
+
+#a.insert(12, 'C++')
+
+#print(list(enumerate(a)))
+
+#a.pop()
+
+#print(list(enumerate(a)))
+
+a.reverse()
+print(a)
