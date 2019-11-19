92dc809b36ad05246e1f8e4a289341212d16b707
Saurav5 <sauravdash45@gmail.com>
2019-11-20 02:34:10 +0530

    sets

---
+++ Saurav/Datatypes/number/sets.py
@@ -0,0 +1,21 @@
+
+abc = set(['a','b' ,'c','b','a'])
+
+#print(abc,type(abc))
+
+a =[1,2,3,4,5]
+b =[5,6]
+
+#print(set(a).union(set(b)))
+
+#print(set(a).intersection(set(b)))
+
+c = set((1,2,3,4,5))
+
+c.update(set((5,6,7)))
+
+print(c)
+
+d = set({"Saurav", "Dash", "Saurav"})
+
+print(set(d))
